4d5de69b99d7d621a06f52ba915c16a618a0e878
Sergey Zelenov <S.Zelenov23@mail.ru>
2023-04-23 23:11:46 +0300

    Задача 19

---
+++ Задача 19.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+string n = Console.ReadLine();    
+int number = n.Length;
+    if (number==5)
+    {
+        if (n[0]==n[4] && n[1]==n[3])
+        {
+        Console.WriteLine("Ура, это палиндром!!!)");
+        }
+        else
+        {
+        Console.WriteLine("Увы, это не палиндром)");   
+        }
+    }
+    else
+    {
+    Console.WriteLine("Это не пятизначное число");
+    }
+
+
