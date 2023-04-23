7eb78a5ff378b6013bc55341f9b64bf3547520ed
ZelenovSV <130816124+ZelenovSV@users.noreply.github.com>
2023-04-23 23:14:13 +0300

    Add files via upload

---
+++ Задача 23.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+    int number = int.Parse(Console.ReadLine());
+    int count = 0;
+    double NewNumber = 0;
+    while (count<number)
+    {
+    NewNumber=count*count*count;
+    Console.WriteLine($"{NewNumber}");
+    count++;
+    }
+    
